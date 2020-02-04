083d379e90bb16c1ae11b5c9e294a413ee04b836
Goncaloferreira96 <60395280+Goncaloferreira96@users.noreply.github.com>
2020-02-04 14:19:07 +0000

    Add files via upload
    Exercício1-FTIM

---
+++ Exercicio1/Exercicio1/Exercicio1.cpp
@@ -0,0 +1,15 @@
+// Exercicio1.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Introduza um número para soma: ";
+	int num1;
+	cin >> num1;
+	cout << "Introduza outro número para soma: ";
+	int num2;
+	cin >> num2;
+	cout << "A soma dos número que introduziu é: " << num1 + num2 << endl;
+}
